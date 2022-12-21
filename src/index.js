02266aab5db303b9c4dad81e55dc9f7a57da6c1e
ehsan mahmoodi <mahmoodie24m@gmail.com>
2022-12-21 20:09:43 +0330

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// style
+import './assets/css/index.css';
+// app
+import App from './app/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
